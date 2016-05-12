91f0af9fa3a1e37ea3e4adb319ffb7371f0d92ed
Fabrizio <fabrizio.rapelli@dovetail.ie>
2016-05-12 13:00:37 +0100

    missing package, removed livereload

--- gulp/styles.js
+++ gulp/styles.js
@@ -5,7 +5,7 @@ import sass from "gulp-sass";
 import path from "path";
 import sourcemaps from "gulp-sourcemaps";
 import notify from "gulp-notify";
-import livereload from 'gulp-livereload';
+//import livereload from 'gulp-livereload';
 
 const sources = ["src/styles/main.scss","src/styles/vendor.scss"];
 
@@ -16,10 +16,11 @@ gulp.task("styles", () => {
         .on("error", notify.onError({title: "styles"}))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest(".tmp/styles"))
-        .pipe(livereload());;
+    //    .pipe(livereload());
+    ;
 });
 
 gulp.task("styles:watch", () => {
-    livereload.listen();
+    //livereload.listen();
     return gulp.watch(sources, ["styles"/*,'electron:reload'*/]);
 });
